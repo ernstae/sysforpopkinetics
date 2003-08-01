24cd778d8f8d51b676362568b3e4cdd3521ded30
Honda <Honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2003-08-01 19:06:35 +0000

    *** empty log message ***

--- spk/aspk/compiler/libcommon/SpkMLToCpp.h
+++ spk/aspk/compiler/libcommon/SpkMLToCpp.h
@@ -83,21 +83,78 @@ extern SymbolTable *gSpkExpSymbolTable;
  */
 extern xercesc::DOMDocument *gSpkExpTree;
 
+/**
+ * ClientTranslator class defines the interfaces
+ * to be implemented by sublasses specific to clients (ex. NONMEM, SAAM2...).
+ *
+ * These interfaces include a method to initiate the process of reading an
+ * SpkML document and convert it to C++ source code files,
+ * ways to acquire the names of these C++ files created and so on.
+ */
 class ClientTranslator
 {
  public:
+  /**
+   * Collect and assemble information mined in the tree necessarily
+   * to generate a SPK driver file and an SpkModel definition
+   * files.
+   *
+   * When process is completed successfully, the pointer returned 
+   * by getDriverFilename() shall point to a character array containing
+   * a path followed by a filename indicating the C++ source
+   * code file for a SPK driver.  In addition, the vector returned
+   * by getModelFilenameList() shall contain character arrays, each
+   * specifies a path followed by a filename indicating the C++ 
+   * source code file containing a portion of SpkModel subclass
+   * definition.
+   * 
+   * When process is completed successfully, two sets of information are
+   * expected, but not required, to be assembled: A data set returned by
+   * getSpkParameters() and a data set returned by getClientParameters().
+   * The first set is composed in the form of FitParameters data structure.
+   * It has place holders like "popParIn" for the initial value for
+   * the population parameter.  The second set is in an arbitrary form (void*).
+   * It can contain any value in any form with any names for convenience.
+   * This set will be primarily used for debugging purpose.
+   * 
+   * @param tree is the DOMDocument-based tree representation
+   * of an SpkML input document.
+   */
+  virtual void translate ( xercesc::DOMDocument * tree ) = 0;
+
+  /**
+   * @return a FitParameters data structured object that may or may
+   * not have valid values associated with its declared variables.
+   */
   virtual const struct FitParameters * getSpkParameters() const = 0;
+
+  /**
+   * @return an arbitrary object that may or may not contain 
+   * a set of information gathered during the translation process
+   * and specific to the client.
+   */
   virtual const void * getClientParameters() const = 0;
-  virtual void assemble ( xercesc::DOMDocument * tree ) = 0;
-  virtual void emit     ( xercesc::DOMDocument * tree ) = 0;
+
+  /**
+   * @return a character array containing a path followed by the
+   * filename associated with the C++ source code file definining
+   * a SPK driver.
+   */
   virtual const char * getDriverFilename() const = 0;
+
+  /*
+   *
+   * @return a vector of character arrays contaings paths followed by
+   * the filenames associated with the C++ source code files defining
+   * a SpkModel subclass.
+   */
   virtual const std::vector< const char * > getModelFilenameList() const = 0;
 };
 
 /**
- * The purpose of this class serves as the interface to
- * invoking a SpkML->C++ translation process and
- * obtaining the resulting C++ source code file (path) names.
+ * SpkMLToCpp class packages the interfaces to the 
+ * SpkML document -> C++ source code translation
+ * related processes.
  * 
  */
 class SpkMLToCpp
@@ -124,7 +181,6 @@ class SpkMLToCpp
   ClientTranslator          * createTranslator  ( enum client::type ) const;
 
   const char                * inputSpkML;
-
   enum client::type           who;
   ClientTranslator          * client_translator;
   xercesc::XercesDOMParser  * parser;
