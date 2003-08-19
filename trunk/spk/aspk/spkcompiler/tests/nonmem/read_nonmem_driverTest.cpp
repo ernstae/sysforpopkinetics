512a5b55de5a828faf196b7c5276c75636ddbdfe
Honda <Honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2003-08-19 18:10:15 +0000

    *** empty log message ***

---
+++ trunk/spk/aspk/spkcompiler/tests/nonmem/read_nonmem_driverTest.cpp
@@ -0,0 +1,264 @@
+#include <iostream>
+#include <valarray>
+#include <fstream>
+
+#include "read_nonmem_driverTest.h"
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TextTestResult.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
+
+#include <spk/printInMatrix.h>
+#include <read_nonmem_driver.h>
+
+using namespace std;
+using namespace CppUnit;
+using namespace xercesc;
+
+void read_nonmem_driverTest::setUp()
+{
+}
+void read_nonmem_driverTest::tearDown()
+{
+}
+
+
+void read_nonmem_driverTest::test()
+{
+  char input[] = "NonmemTranslatorTestInput.xml";
+  XMLPlatformUtils::Initialize();
+  XercesDOMParser * parser = new xercesc::XercesDOMParser;
+  parser->setValidationScheme( XercesDOMParser::Val_Auto );
+  parser->setDoNamespaces(true );
+  parser->setDoSchema(true );
+  parser->setValidationSchemaFullChecking(true);
+  parser->setCreateEntityReferenceNodes(true);
+
+  try
+  {
+    ifstream ifs( input );
+    if( !ifs.good() )
+      {
+        XMLPlatformUtils::Terminate();
+        char buf[256];
+        sprintf( buf, "Failed to open %s!\n", input);
+        fprintf( stderr, buf );
+        exit(-1);
+      }
+    else
+      ifs.close();
+    parser->parse(input);
+  }
+  catch( const XMLException& e )
+  {
+    XMLPlatformUtils::Terminate();
+    char buf[256];
+    sprintf( buf, "An error occurred during parsing\n   Message: %s\n", C(e.getMessage() ) );
+    fprintf( stderr, buf );
+    exit( -1 );
+  }
+
+  catch( const DOMException& e )
+  {
+      const unsigned int maxChars = 2047;
+      XMLCh errText[maxChars + 1];
+      char buf[256];
+      sprintf( buf, "DOM Error during parsing \"%s\"\nDOMException code is: %d\n",
+               input, e.code );
+      fprintf( stderr, buf );
+      exit(-1);
+
+      if (DOMImplementation::loadDOMExceptionMsg(e.code, errText, maxChars))
+      {
+        XMLPlatformUtils::Terminate();
+        char buf[256];
+        sprintf( buf, "Message is: %s.\n %d, %s\n", C(errText), __LINE__, __FILE__ );
+        fprintf( stderr, buf );
+        exit( -1 );
+      }
+  }
+  catch( ... )
+  {
+    XMLPlatformUtils::Terminate();
+    char buf[128];
+    sprintf( buf, "An unknown error occurred during parsing.\n %d, %s\n", __LINE__, __FILE__ );
+    fprintf( stderr, buf );
+    exit( -1 );
+  }
+
+  DOMElement * driverNode = dynamic_cast<DOMElement*>( 
+	     parser->getDocument()
+	     ->getElementsByTagName( XMLString::transcode( "driver" ) )->item(0) );
+  const XMLCh * xml_analysis = 
+    dynamic_cast<DOMElement*>( parser->getDocument()->getElementsByTagName( XMLString::transcode( "content" ) )->item(0) )
+    ->getAttribute( XMLString::transcode( "analysis" ) );
+
+  enum SpkParameters::Analysis analysis 
+    = ( XMLString::equals( xml_analysis, XMLString::transcode( "population" ) )? 
+			   SpkParameters::POPULATION : SpkParameters::INDIVIDUAL );
+  CPPUNIT_ASSERT_MESSAGE( "The analysis type is supposed to be SpkParameters::POPULATION!",
+			  analysis == SpkParameters::POPULATION );
+
+  struct NonmemParameters nonmem;
+
+  struct SpkParameters spk;
+
+  read_nonmem_driver( driverNode, spk, nonmem );
+
+  const int nIndividuals = 12;
+
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "#of individuals is supposed to be 12!", 
+			  nIndividuals, spk.nIndividuals );
+  // <optimization> section
+  CPPUNIT_ASSERT_MESSAGE( "The objective is supposed to be FIRST_ORDER!", 
+			  spk.objective == FIRST_ORDER );
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "(pop) epsilon is supposed to be 0.001!",
+			  0.001, spk.popEpsilon );
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "(pop) mitr is supposed be 450!",
+			  450, spk.popMaxItr );
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "(pop) trace is supposed to be 4!",
+			  4, spk.popTrace );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) popParOut should be requested!",
+			  spk.isPopParOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) popObjOut should be requeseted!", 
+			  spk.isPopObjOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) popObj_popParOut should be requeseted!", 
+			  spk.isPopObj_popParOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) popObj_popPar_popParOut should be declined!", 
+			  !spk.isPopObj_popPar_popParOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) warm start is supposed to be requested!",
+			  spk.isPopWarmStart );
+
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "(ind) epsilon is supposed to be 0.01!",
+			  0.01, spk.indEpsilon );
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "(ind) mitr is supposed be 100!",
+			  100, spk.indMaxItr );
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "(ind) trace is supposed to be 0!",
+			  0, spk.indTrace );
+  CPPUNIT_ASSERT_MESSAGE( "(ind) indParOut should be requested!",
+			  spk.isIndParOut);
+  CPPUNIT_ASSERT_MESSAGE( "(ind) indObjOut should be requeseted!", 
+			  spk.isIndObjOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) indObj_indParOut should be requeseted!", 
+			  spk.isIndObj_indParOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) indObj_indPar_indParOut should be declined!", 
+			  !spk.isIndObj_indPar_indParOut );
+  CPPUNIT_ASSERT_MESSAGE( "(ind) warm start is supposed to be requested!",
+			  spk.isIndWarmStart );
+
+  // <statistics> section
+  CPPUNIT_ASSERT_MESSAGE( "(pop) standard error computation is supposed to be requested!",
+			  spk.isPopStderrorOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) correlation computation is supposed to be requested!",
+			  spk.isPopCorrelationOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) covariance computation is supposed to be requested!",
+			  spk.isPopCovarianceOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) coefficient computation is supposed to be requested!",
+			  spk.isPopCoefficientOut );
+  CPPUNIT_ASSERT_MESSAGE( "(pop) confidence interval computation is supposed to be requested!",
+			  spk.isPopConfidenceOut );
+  
+  CPPUNIT_ASSERT_MESSAGE( "(ind) standard error computation is supposed to be requested!",
+			  !spk.isIndStderrorOut );
+  CPPUNIT_ASSERT_MESSAGE( "(ind) correlation computation is supposed to be requested!",
+			  !spk.isIndCorrelationOut );
+  CPPUNIT_ASSERT_MESSAGE( "(ind) covariance computation is supposed to be requested!",
+			  !spk.isIndCovarianceOut );
+  CPPUNIT_ASSERT_MESSAGE( "(ind) coefficient computation is supposed to be requested!",
+			  !spk.isIndCoefficientOut );
+  CPPUNIT_ASSERT_MESSAGE( "(ind) confidence interval computation is supposed to be requested!",
+			  !spk.isIndConfidenceOut );
+
+  int omegaElemNum = 3*(3+1)/2;
+  valarray<double> omega( 0.1, omegaElemNum );
+  valarray<double> omegaActual = nonmem.omegaIn;
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "Omega: #of elements", omegaElemNum, static_cast<int>(omegaActual.size()) );
+
+  for( int i=0; i<omegaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Omega", omega[i], omegaActual[i] );
+
+  valarray<bool> omegaFixed( false, omegaElemNum );
+  valarray<bool> omegaFixedActual = nonmem.omegaFixed;
+  omegaFixed[0] = true;
+  omegaFixed[3] = true;
+  for( int i=0; i<omegaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Omega fixed", omegaFixed[i], omegaFixedActual[i] );
+
+  int sigmaElemNum = 1*(1+1)/2;
+  valarray<double> sigma( 0.3, sigmaElemNum );
+  valarray<double> sigmaActual = nonmem.sigmaIn;
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "Sigma: #of elements", sigmaElemNum, static_cast<int>(sigmaActual.size()) );
+
+  for( int i=0; i<sigmaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "Sigma", sigma[i], sigmaActual[i] );
+
+  int etaElemNum = 3;
+  valarray<double> eta( 0.0, etaElemNum );
+  valarray<double> etaActual = nonmem.etaIn;
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "eta: #of elements", etaElemNum, static_cast<int>(etaActual.size()) );
+
+  for( int i=0; i<etaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "eta", eta[i], etaActual[i] );
+
+  valarray<bool> etaFixed( false, etaElemNum );
+  valarray<bool> etaFixedActual = nonmem.etaFixed;
+  etaFixed[1] = true;
+
+  for( int i=0; i<etaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "eta fixed", etaFixed[i], etaFixedActual[i] );
+
+  int thetaElemNum = 3;
+  valarray<double> thetaIn( 0.1, thetaElemNum );
+  valarray<double> thetaInActual = nonmem.thetaIn;
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "thetaIn: #of elements", 
+				thetaElemNum, static_cast<int>(thetaInActual.size()) );
+
+  for( int i=0; i<thetaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "thetaIn", thetaIn[i], thetaInActual[i] );
+  
+  valarray<bool> thetaFixed(false, thetaElemNum);
+  thetaFixed[0] = true;
+
+  valarray<bool> thetaFixedActual = nonmem.thetaFixed;
+  for( int i=0; i<thetaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "thetaFixed", thetaFixed[i], thetaFixedActual[i] );
+
+  valarray<double> thetaLow( -0.1, thetaElemNum );
+  valarray<double> thetaLowActual = nonmem.thetaLow;
+
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "thetaLow: #of elements", 
+				thetaElemNum, static_cast<int>(thetaLowActual.size()) );
+
+  for( int i=0; i<thetaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "thetaIn", thetaLow[i], thetaLowActual[i] );
+
+
+  valarray<double> thetaUp( 1.0, thetaElemNum );
+  valarray<double> thetaUpActual = nonmem.thetaUp;
+  CPPUNIT_ASSERT_EQUAL_MESSAGE( "thetaUp: #of elements", 
+				thetaElemNum, static_cast<int>(thetaUpActual.size()) );
+
+  for( int i=0; i<thetaElemNum; i++ )
+    CPPUNIT_ASSERT_EQUAL_MESSAGE( "thetaUp", thetaUp[i], thetaUpActual[i] );
+
+  delete parser;
+  XMLPlatformUtils::Terminate();
+}
+
+CppUnit::Test * read_nonmem_driverTest::suite()
+{
+  CppUnit::TestSuite *suiteOfTests = new CppUnit::TestSuite( "read_nonmem_driverTest" );
+
+  suiteOfTests->addTest( new CppUnit::TestCaller<read_nonmem_driverTest>("test",
+						    &read_nonmem_driverTest::test ) );
+
+   return suiteOfTests;
+}
+
---
+++ trunk/spk/aspk/spkcompiler/tests/nonmem/read_nonmem_driverTest.h
@@ -0,0 +1,22 @@
+/**
+ * \file read_nonmem_driverTest.h
+ * \brief A test suite for the read_nonmem_driver() function.
+ */
+#ifndef TEST_READ_NONMEM_DRIVER_H
+#define TEST_READ_NONMEM_DRIVER_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestSuite.h>
+#include "nonmem/read_nonmem_driver.h"
+
+class read_nonmem_driverTest : public CppUnit::TestFixture {
+
+public:
+    static CppUnit::Test * suite();
+    virtual void setUp();
+    virtual void tearDown();
+
+    void test();
+};
+
+#endif
