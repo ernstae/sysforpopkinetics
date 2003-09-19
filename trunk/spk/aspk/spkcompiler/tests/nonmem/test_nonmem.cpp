fe4e75c0a2540f52839cfc2e44bea9af1f676147
Honda <Honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2003-09-19 07:16:01 +0000

    Made it capable of running a subset of the entire collection of tests.

--- trunk/spk/aspk/spkcompiler/tests/nonmem/test_nonmem.cpp
+++ trunk/spk/aspk/spkcompiler/tests/nonmem/test_nonmem.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <map>
 #include <cppunit/TextTestResult.h>
 #include <cppunit/ui/text/TestRunner.h>
 
@@ -6,19 +8,54 @@
 #include "read_nonmem_dataTest.h"
 #include "read_nonmem_modelTest.h"
 #include "NonmemExpXlatorTest.h"
+#include "emit_nonmem_driverTest.h"
 
 using namespace std;
 using namespace CppUnit;
 
 int main( int argc, const char * argv[] )
 {
+  map<string, CppUnit::Test*> master;
+  vector<CppUnit::Test*> subset;
+
+  master[ "NonmemExpXlatorTest" ]    =  NonmemExpXlatorTest::suite();
+  master[ "read_nonmem_driverTest" ] = read_nonmem_driverTest::suite();
+  master[ "read_nonmem_dataTest" ]   = read_nonmem_dataTest::suite();
+  master[ "read_nonmem_modelTest" ]  = read_nonmem_modelTest::suite();
+  master[ "emit_nonmem_driverTest" ] = emit_nonmem_driverTest::suite();
+
+  if( argc == 1 )
+    {
+      map<string, CppUnit::Test*>::const_iterator p = master.begin();
+      for( p; p != master.end(); p++ )
+	subset.push_back( p->second );
+    }
+  else
+    {
+      for( int i=1; i<argc; i++ )
+	{
+	  map<string, CppUnit::Test*>::const_iterator p = master.find( argv[i] );
+	  if( p != master.end() )
+	    {
+	      subset.push_back( p->second );
+	    }
+	  else
+	    {
+	      fprintf( stderr, "!!! Can't find %s in the master list (type?) !!! \n",
+		       argv[i] );
+	    }
+	}
+    }
+
+
   CppUnit::TextUi::TestRunner runner;
 
-  runner.addTest( NonmemExpXlatorTest::suite() );
-  runner.addTest( read_nonmem_driverTest::suite() );
-  runner.addTest( read_nonmem_dataTest::suite() );
-  runner.addTest( read_nonmem_modelTest::suite() );
-  
+  int n = subset.size();
+  for( int i=0; i<n; i++ )
+    {
+      runner.addTest( subset[i] );
+    }
+
   runner.run();
 
   return 0;
