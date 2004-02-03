f0a3048fb22cedce1346173e7b07a88f42f9a12e
honda <honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-02-03 23:13:13 +0000

    *** empty log message ***

--- trunk/spk/aspk/spkcompiler/libspkcompiler/lower.h
+++ trunk/spk/aspk/spkcompiler/libspkcompiler/lower.h
@@ -1,8 +1,17 @@
+/**
+ * @file lower.h
+ * Declare the lower() function.
+ */
 #ifndef LOWER_H
 #define LOWER_H
 
 #include <string>
 
+/**
+ * @param source The source string (length >= 0).
+ * @return       A new string object equivalent to the source with all 
+ *               lower case letters converted to the lower case.
+ */
 const std::string lower( const std::string& mix );
 
 #endif
