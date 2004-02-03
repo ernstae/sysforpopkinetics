26c84b087059b359ab21bd6ccaf8e6c5f62672e0
honda <honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-02-03 23:13:13 +0000

    *** empty log message ***

--- spk/aspk/spkcompiler/libspkcompiler/upper.h
+++ spk/aspk/spkcompiler/libspkcompiler/upper.h
@@ -1,6 +1,16 @@
+/**
+ * @file upper.h
+ * Declare the upper() function.
+ */
 #ifndef UPPER_H
 #define UPPER_H
 
-const std::string upper( const std::string& mix );
+#include <string>
+/**
+ * @param source The source string (length >= 0).
+ * @return       A new string object equivalent to the source with all 
+ *               lower case letters converted to the upper case.
+ */
+const std::string upper( const std::string& source );
 
 #endif
