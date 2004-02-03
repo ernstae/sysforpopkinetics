363a9f1feb29d54e2f2116954a807511d428bf28
honda <honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-02-03 23:10:51 +0000

    A test for lower().

---
+++ spk/aspk/spkcompiler/tests/common/lowerTest.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TextTestResult.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+
+#include <spkcompiler/lower.h>
+#include "lowerTest.h"
+
+using namespace std;
+using namespace CppUnit;
+
+
+
+void lowerTest::setUp()
+{
+}
+
+void lowerTest::tearDown()
+{
+}
+
+void lowerTest::testMix()
+{
+  char * AAA[] = { "AAA", "AAa", "AaA", "Aaa", "aAA", "aAa", "aaA" };
+
+  for( int i=0; i<7; i++ )
+    {
+      string aaa = lower( AAA[i] );
+      CPPUNIT_ASSERT( aaa == string( "aaa" ) );
+    }
+ 
+}
+void lowerTest::testEmpty()
+{
+  CPPUNIT_ASSERT( lower("") == string("") );
+}
+
+CppUnit::Test * lowerTest::suite()
+{
+  CppUnit::TestSuite *suiteOfTests 
+    = new CppUnit::TestSuite( "lowerTest" );
+  suiteOfTests->addTest( new CppUnit::TestCaller<lowerTest>
+			 ("testMix", &lowerTest::testMix ) );
+  suiteOfTests->addTest( new CppUnit::TestCaller<lowerTest>
+			 ("testEmpty", &lowerTest::testEmpty ) );
+
+ return suiteOfTests;
+}
+
