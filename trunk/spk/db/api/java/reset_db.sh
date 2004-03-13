a4b14de97bac93b75db70e14ff8fd7c9efeb11bc
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-03-13 19:58:28 +0000

    .

--- trunk/spk/db/api/java/reset_db.sh
+++ trunk/spk/db/api/java/reset_db.sh
@@ -1,6 +1,7 @@
 D=../../admin
 
 TMP_NAME="/tmp/junk$$";
+echo $HOST
 echo 'use spktest;' > $TMP_NAME
-cat $TMP_NAME $D/drop.sql   | mysql --force -ptester -utester > /dev/null 2>&1
-cat $TMP_NAME $D/schema.sql | mysql --force -ptester -utester
+cat $TMP_NAME $D/drop.sql   | mysql --force -h $HOST -ptester -utester > /dev/null 2>&1
+cat $TMP_NAME $D/schema.sql | mysql --force -h $HOST -ptester -utester
