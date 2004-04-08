03a41891c8295607595371296ec224d6c47ade8f
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-04-08 21:21:52 +0000

    Continued development of the compiler daemon.

--- trunk/spk/aspk/daemon/spkcmpd.pl
+++ trunk/spk/aspk/daemon/spkcmpd.pl
@@ -1,4 +1,112 @@
 #!/usr/bin/perl -w
+########################################################################
+#
+# This file is part of the System for Population Kinetics (SPK), which
+# was developed with support from NIH grants RR-12609 and P41-
+# EB001975. Please cite these grants in any publication for which this
+# software is used and send a notification to the address given above.
+#
+# SPK is Copyright (C) 1998-2003, by the University of Washington,
+# Resource Facility for Population Kinetics, and is made available as
+# free open source software under the terms of the University of
+# Washington Free-Fork License as a public service.  A copy of the
+# License can be found in the COPYING file in the root directory of this
+# distribution or can be obtained from
+#     Resource Facility for Population Kinetics
+#     Department of Bioengineering Box 352255
+#     University of Washington
+#     Seattle, WA 98195-2255
+########################################################################
+
+=head1 NAME
+
+spkcmpd.pl -- the SPK Compiler Daemon
+
+=head1 SYNOPSIS
+
+spkcmpd.pl database host dbuser dbpasswd
+
+=head1 ABSTRACT
+
+The SPK Compiler Daemon runs continuously on the ASPK server. It
+selects jobs from the compiler queue, forks the spkcompiler to
+process them, and inserts them into the run qeueue,  In cases where
+a job fails to compile, the daemon records the error report and
+the 'end' status in the database.
+
+=head1 DESCRIPTION
+
+The program expects the following arguments:
+
+    $database
+        The name of the SPK database
+    $host
+        The host on which the SPK database resides
+    $dbuser
+        A database username which has read/write access to the 
+        job table
+    $dbpasswd
+        The password associated with the username
+
+The first think that spkcmp.pl does after starting up is to call
+Proc::Daemon::Init to make it into a daemon, by shedding its 
+inheirited environment and becoming a direct child of the system
+init process.
+
+It then opens the system log so that it has a place to record progress
+and error messages.
+
+It attempts to create a lock-file if one does not already exist. The
+method that is used assures that the process of query and creation
+is atomic.  If the lock-file already exists, the program writes an
+error message to the system log and terminates, because only one 
+copy of spkcmpd.pl can be allowed run at any given time.
+
+Next, it opens the database.
+
+The program designates itself to be a process group leader. This
+way it will be able to send signals to all of its descendents without
+having to know their PIDs. 
+
+The "stop" subroutine is designated to catch the TERM signal,
+when it is received. As explained below, this will allow for
+an orderly shutdown of the daemon and its sub-processes.
+
+The last major step in the start-up sequence is to select from the
+database all jobs with a state_code field set to 'cmp'.  These
+jobs, if any exist, had been in the process of being compiled when 
+the daemon last shut down.  All such jobs are rerun.
+
+At this point, the program enters an endless loop from which it will
+escape only upon receipt of a signal. It queries the
+database to discover whether or not a job has been added to the compiler
+queue.  If so, a copy of spkcompiler is started as an independent
+sub-process, working in its own directory on input provided by the
+job. The daemon then checks to see if any child processes have 
+terminated. If so, it moves them either to the run queue or to 
+"end" status, depending on whether the compilation was successful
+or not.  The daemon sleeps a second, before continuing the
+loop.
+
+The normal way in which spkcmp.pl is terminated is by using the
+Unix "kill" command to send it the TERM signal.  When TERM is received,
+execution is passed to the 'stop' subroutine, which had previously
+been designated to catch this signal.
+
+To avoid a loop, 'stop' sets the signal mask to ignore any
+subsequent TERM signals. It then sends the TERM signal to every process
+in its process group, which consists of the daemon itself and all of its
+descendents. It waits for all sub-processes (which are 
+instances of the spkcompiler) to terminate.  It closes the database
+and the system log, then dies.
+
+=head1 RETURNS
+
+Nothing, because it has no parent (other than init) to which an exit
+code might be returned.  The program does, however, write event messages
+to the system log.
+
+=cut
 
 use strict;
 
@@ -12,7 +120,8 @@ my $host     = shift;
 my $dbuser   = shift;
 my $dbpasswd = shift;
 
-my $compiler_path = "/usr/local/bin/spkcompiler";
+#my $compiler_path = "/usr/local/bin/spkcompiler";
+my $compiler_path = "/usr/local/bin/spkcompilerx";
 my $dbh;
 my $database_open = 0;
 my $service_name = "spkcmpd";
@@ -108,6 +217,9 @@ sub stop {
     # We have received the TERM signal.  So have our children. 
     # Wait as them terminate, and clean up after them.
     
+    local $SIG{'TERM'} = 'IGNORE';
+    kill('TERM', -$$);
+
     my $child_pid;
 
     while (($child_pid = wait()) != -1) {
@@ -115,14 +227,17 @@ sub stop {
     }
     death('info', 'received the TERM signal (normal mode of termination)');
 }
-# designate stop subroutine to catch the Unix TERM signal
-$SIG{"TERM"} = \&stop;
 
 # become a daemon
 Proc::Daemon::Init();
 
 # initialize
 start();
+
+setpgrp(0, 0);
+
+# designate stop subroutine to catch the Unix TERM signal
+$SIG{"TERM"} = \&stop;
 
 # rerun any compiles that were interrupted when we last terminated
 $row_array = &Spkdb::get_cmp_jobs($dbh);
@@ -152,8 +267,7 @@ while(1) {
 	death("emerg", "error reading database: $Spkdb::errstr");
     }
     # check for the death of our compiler child processes
-    $pid_of_deceased_child = waitpid(-1, &WNOHANG);
-    if ($pid_of_deceased_child > 0) {
+    while (($pid_of_deceased_child = waitpid(-1, &WNOHANG)) > 0) {
 	syslog("info", "process with pid=$pid_of_deceased_child terminated normally");
     }
     # sleep for a second
