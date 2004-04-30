f97d1e63a532a3226e2d562d1a9d58cb5a4a0db7
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-04-30 18:08:14 +0000

    Now resetdb.pl takes an argument

--- trunk/spk/db/admin/resetdb.pl
+++ trunk/spk/db/admin/resetdb.pl
@@ -4,11 +4,12 @@ use strict;
 
 use Spkdb ('connect', 'disconnect', 'new_job', 'new_user');
 
+my $schema = shift;
+
 my $rv;
 
 my $tmp_name = "/tmp/junk$$";
 my $admin = ".";
-my $schema = "$admin/schema_1.sql";
 my $drop   = "$admin/drop.sql";
 system "echo 'use spktest;' > $tmp_name";
 system "cat $tmp_name $drop   | mysql --force -ptester -utester > /dev/null 2>&1";
