a8f20896881093db8f44647c21038de9c05165d4
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-05-17 19:33:15 +0000

    Generalized resetdb.pl to work with both database and check argument.

--- spk/db/admin/resetdb.pl
+++ spk/db/admin/resetdb.pl
@@ -4,16 +4,21 @@ use strict;
 
 use Spkdb ('connect', 'disconnect', 'new_job', 'new_user');
 
+@ARGV == 1
+    or die("usage: resetdb.pl sql-file\n");
+
 my $schema = shift;
 
-my $rv;
+my $dbname = "spktest";
+my $dbuser = "tester";
+my $dbpass = "tester";
 
 my $tmp_name = "/tmp/junk$$";
 my $admin = ".";
 my $drop   = "$admin/drop.sql";
-system "echo 'use spktest;' > $tmp_name";
-system "cat $tmp_name $drop   | mysql --force -ptester -utester > /dev/null 2>&1";
-system "cat $tmp_name $schema | mysql --force -ptester -utester";
+system "echo 'use $dbname;' > $tmp_name";
+system "cat $tmp_name $drop   | mysql --force -p$dbpass -u$dbuser > /dev/null 2>&1";
+system "cat $tmp_name $schema | mysql --force -p$dbpass -u$dbuser";
 system "rm $tmp_name";
 
 exit 0;
