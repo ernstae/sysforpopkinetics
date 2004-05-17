a8873cc2596a697ef0d8a307a02ca9ed92b625e0
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-05-17 18:31:54 +0000

    Generalized resetdb.pl, somewhat.

--- trunk/spk/cspk/daemon/resetdb.pl
+++ trunk/spk/cspk/daemon/resetdb.pl
@@ -4,15 +4,17 @@ use strict;
 
 use Spkdb ('connect', 'disconnect', 'new_job', 'new_user');
 
-my $rv;
+my $dbname = "spktest";
+my $dbuser = "tester";
+my $dbpass = "tester";
 
 my $tmp_name = "/tmp/junk$$";
 my $admin = "../../../spk/db/admin";
 my $schema = "$admin/schema_2.sql";
 my $drop   = "$admin/drop.sql";
-system "echo 'use spktest;' > $tmp_name";
-system "cat $tmp_name $drop   | mysql --force -h dbserver -ptester -utester";
-system "cat $tmp_name $schema | mysql --force -h dbserver -ptester -utester";
+system "echo 'use $dbname;' > $tmp_name";
+system "cat $tmp_name $drop   | mysql --force -h dbserver -p$dbpass -u$dbuser";
+system "cat $tmp_name $schema | mysql --force -h dbserver -p$dbpass -u$dbuser";
 system "rm $tmp_name";
 
 exit 0;
