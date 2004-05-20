1511e30b32c15295f8fa5850883376b485b6762e
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-05-20 23:24:18 +0000

    modified resetdb.pl and related schema files to separate table creation from data insertion.

--- spk/db/admin/resetdb.pl
+++ spk/db/admin/resetdb.pl
@@ -5,9 +5,9 @@ use strict;
 use Spkdb ('connect', 'disconnect', 'new_job', 'new_user');
 
 @ARGV == 1
-    or die("usage: resetdb.pl sql-file\n");
+    or die("usage: resetdb.pl data-only-sql-file\n");
 
-my $schema = shift;
+my $data = shift;
 
 my $dbname = "spktest";
 my $dbuser = "tester";
@@ -16,9 +16,13 @@ my $dbpass = "tester";
 my $tmp_name = "/tmp/junk$$";
 my $admin = ".";
 my $drop   = "$admin/drop.sql";
+my $schema = "$admin/schema.sql";
+
 system "echo 'use $dbname;' > $tmp_name";
 system "cat $tmp_name $drop   | mysql --force -p$dbpass -u$dbuser > /dev/null 2>&1";
 system "cat $tmp_name $schema | mysql --force -p$dbpass -u$dbuser";
+system "cat $tmp_name $data   | mysql --force -p$dbpass -u$dbuser";
+
 system "rm $tmp_name";
 
 exit 0;
