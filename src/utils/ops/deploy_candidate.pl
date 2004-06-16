78b49f42b2978034cd84e338ee01b8a43f6173ff
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-06-16 00:50:29 +0000

    Added documentation to deploy_candidate.pl and stage_candidate.pl

--- src/utils/ops/deploy_candidate.pl
+++ src/utils/ops/deploy_candidate.pl
@@ -6,6 +6,41 @@ use File::Path;
 
 use cand("make_directory");
 
+=head1 NAME
+
+    deploy_candidate.pl -- deploy an SPK candidate system to production
+
+=head1 SYNPOSIS
+
+    deploy_candidate.pl
+
+=head1 ABSTRACT
+
+    The deploy_candidate.pl operations utility is part of the process of
+    moving an SPK system from test into production. It takes the most
+    recent candidate and moves it to production.
+
+=head1 RUNNING
+
+    To run this program, you must satisfy these conditions:
+    -  Have a terminal window or ssh window on whitechuck
+    -  Run as root in the root environment. One way to achieve this is
+       to log in as an ordinary user, then upgrade with 
+            su -
+        Note that the "-" sign is necessary. Without it, you will be root
+        but you will be running in the ordinary user's environment.
+
+=head1 DESCRIPTION
+
+    See documentation for stage_candidate.pl for an overview of the test
+    and installation platforms for the RFPK installation of SPK, and the
+    process of moving systems from test to production, the directory
+    structure used for storing candidate systems, the contents and the
+    format of files.
+
+=cut
+
+
 my $ops_root = "/usr/local/spk/ops";
 my $candidate_dir = "$ops_root/candidate";
 
@@ -59,7 +94,6 @@ foreach my $d ("aspkserver", "cspkserver") {
     my $ddir = "$d:/tmp/usr/local";
     foreach my $f (<$sdir/*>) {
 	my @args = ($scp_command, "-r", "$f", $ddir);
-	print "@args", "\n";
         system(@args);
         my $exit_status = $? >> 8;
 	if ($exit_status != 0) {
