1ed0a5d0697aca98745d274a20edfdaccea7c7fd
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-06-18 00:22:33 +0000

    Added new utils.

---
+++ src/utils/ops/dump_spkdb.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+=head1 NAME
+    
+    dump_spkdb.pl -- take a symbolic dump of the production database
+
+=head1 SYNOPSIS
+
+    dump_spkdb.pl
+
+=head1 ABSTRACT
+
+    This program takes a symbolic dump of the production database, so that
+    the a clone of the database can be constructed in the test database 
+    usiing load_spktest.pl.
+
+    Warning: the output of this program may contain inconsistencies if a transaction
+    was in progress at the time the program was run.  The output should not, therefore,
+    be relied upon for rebuilding the production database in case it is damaged.
+    The principal use of this program is to create test databases as clones of 
+    the production database, using the program load_spktest.pl
+
+=head1 SYNOPSIS
+
+    dump_spkdb.pl
+
+=head1 DESCRIPTION
+
+    This program is designed to be run from the database server. It is not necessary
+    for the user have root privileges.  It does not alter the production database
+    in any way.  It produces its output in the current directory.
+
+=head2 OUTPUT FILE CONTENTS
+
+    The output of this program consists of three files of sql statements:
+    - schema:   'create table' statements
+    - basedata: 'insert' statements for inserting all data into the non-transactional
+                table; namely, end, method, and state
+    - userdata: 'insert' statements for inserting all data into the rest of the tables
+
+=head2 OUTPUT FILE NAMES
+
+    The output files have names with the following pattern:
+
+    spkdb-yyyy-mm-dd-hhmm-ss-schema.sql
+    spkdb-yyyy-mm-dd-hhmm-ss-basedata.sql
+    spkdb-yyyy-mm-dd-hhmm-ss-userdata.sql
+
+=head2 DEPENDENCIES
+
+    The program depends on a MySQL utility, mysqldump, to do the hard work.
+
+=head1 BUGS
+
+    As stated above, there may be inconsistencies in the output if transactions
+    are in progress at the time the dump is taken.
+
+=cut
+
+my $mysqldump = "/usr/bin/mysqldump";
+my $user = "tester";
+my $pass = "tester";
+#my $db = "spkdb";
+my $db = "spktest";
+
+my ($sec, $min, $hour, $mday, $mon, $year) = localtime;
+my $date = sprintf "%04d-%02d-%02d-%02d%02d-%02d", $year+1900, $mon+1, $mday, $hour, $min, $sec;
+
+my $prefix = "$db-$date";
+my $schema   = "$prefix-schema.sql";
+my $basedata = "$prefix-basedata.sql";
+my $userdata = "$prefix-userdata.sql";
+
+system("mysqldump -u$user -p$pass  -d $db > $schema") == 0
+    or die "Could not dump the schema\n";
+system("mysqldump -u$user -p$pass -tc $db --tables end method state > $basedata") == 0
+    or die "Could not dump the basedata tables\n";
+system("mysqldump -u$user -p$pass  -d $db --tables dataset history job model user > $userdata") == 0
+    or die "Could not dump the userdata tables\n";
+
+
+
--- src/utils/ops/stage_candidate.pl
+++ src/utils/ops/stage_candidate.pl
@@ -3,7 +3,6 @@
 use strict;
 use English;
 use File::Path;
-use cand("stage_directory", "make_directory");
 
 =head1 NAME
 
@@ -129,6 +128,34 @@ my $dir_prefix = "spk-";
 
 my $logrotate_command = "/usr/sbin/logrotate";
 
+my $mkdir_command = "/bin/mkdir";
+my $scp_command = "/usr/bin/scp";
+
+sub stage_directory {
+    my $host = shift;
+    my $path = shift;
+    my $source = "$host:$path/spktest";
+    my $dest = "$host$path";
+    my @args = ($scp_command, "-r", $source, $dest);
+    print "@args", "\n";
+    system(@args);
+    my $exit_status = $? >> 8;
+    if ($exit_status != 0) {
+	die "'scp -r $source $dest' failed\n";
+    }
+   rename "$dest/spktest", "$dest/spkprod";
+}
+
+sub make_directory {
+    my $path = shift;
+    my @args = ($mkdir_command, "-p", $path);
+     system(@args);
+    my $exit_status = $? >> 8;
+    if ($exit_status != 0) {
+	die "Could not make directory '$path'\n";
+    }
+}
+
 $EFFECTIVE_USER_ID == 0 
     or die "You must be root to run this program\n";
 
@@ -192,6 +219,4 @@ stage_directory "aspkserver", "/usr/local/lib";
 stage_directory "cspkserver", "/usr/local/bin";
 stage_directory "cspkserver", "/usr/local/include";
 stage_directory "cspkserver", "/usr/local/lib";
-
-
 
