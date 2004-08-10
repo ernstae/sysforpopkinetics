712452b66ccbc4ea725b05881a5a0d07ed49a235
honda <honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-08-10 16:14:06 +0000

    Added ind_withoutIDTest and ind_simTest.

---
+++ spk/aspk/spkcompiler/tests/nonmem/ind_withoutIDTest.h
@@ -0,0 +1,37 @@
+/**
+ * \file ind_withoutIDTest.h
+ * \brief Tests the NonmemTranslator's ability to handle
+ * data sets containing no ID.
+ */
+#ifndef IND_WITHOUTID_TEST_H
+#define IND_WITHOUTID_TEST_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestSuite.h>
+
+#include <xercesc/dom/DOMDocument.hpp>
+
+class ind_withoutIDTest : public CppUnit::TestFixture {
+
+  xercesc::DOMDocument *source;
+  xercesc::DOMDocument *data;
+  xercesc::DOMDocument *report;
+  
+  bool okToClean;
+public:
+    static CppUnit::Test * suite();
+    virtual void setUp();
+    virtual void tearDown();
+
+    void createDataML();
+    void createSourceML();
+    void parse();
+    void testNonmemPars_h();
+    void testIndDataClass();
+    void testDataSetClass();
+    void testPredClass();
+    void testDriver();
+    void testReportML();
+};
+
+#endif
