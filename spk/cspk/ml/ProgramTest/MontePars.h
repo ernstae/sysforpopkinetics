3ae5776a7e55d9bb0369ef558e177f4e2ecfa14b
Brad Bell <bradbell@seanet.com>
2004-08-23 18:38:23 +0000

    new automatically generated version

--- spk/cspk/ml/ProgramTest/MontePars.h
+++ spk/cspk/ml/ProgramTest/MontePars.h
@@ -1,13 +1,19 @@
+//==============================================================================
+// 
+// BRAD_LINEAR_MODEL
+// 
+// The namespace MontePars exports the values needed by monteDriver.cpp.
+// 
+// The user requested the population analysis.
+// 
+//==============================================================================
 #ifndef MONTEPARS_H
 #define MONTEPARS_H
 
-namespace MontePars
-{
-  enum Method { analytic, grid, monte };
-  
-  const Method method = monte;
-
-  const int numberEval = 100;
+namespace MontePars{
+   enum METHOD { monte, analytic, grid };
+   const enum METHOD method = monte;
+   const int numberEval = 10000;
 };
 
 #endif
