05b00c1ccdf1bc4f80043cfebccd8744ff5ba675
honda <honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-09-22 16:31:06 +0000

    *** empty log message ***

--- spk/cspk/spk/lib/src/popStatistics.h
+++ spk/cspk/spk/lib/src/popStatistics.h
@@ -62,4 +62,15 @@ void popStatistics(
          SPK_VA::valarray<double>*       popParCIOut
                );
 
+void popStatistics( const SPK_VA::valarray<double>& y,
+                    const SPK_VA::valarray<double>& alp,
+                    const SPK_VA::valarray<double>& indObj_alp,
+                    const SPK_VA::valarray<double>& popObj_alp_alp,
+                    const PopCovForm              & formulation,
+                    SPK_VA::valarray<double>      * alpCovOut,
+                    SPK_VA::valarray<double>      * alpSEOut,
+                    SPK_VA::valarray<double>      * alpCorOut,
+                    SPK_VA::valarray<double>      * alpCVOut,
+                    SPK_VA::valarray<double>      * alpCIOut );
+
 #endif
