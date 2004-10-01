e260bed3f04608f4aca5548101719ad584719bea
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-10-01 23:28:29 +0000

    Modified stage_candidate.pl to use versions rather than dates.

--- trunk/src/utils/ops/stage_candidate.pl
+++ trunk/src/utils/ops/stage_candidate.pl
@@ -5,6 +5,124 @@ use strict;
 use English;
 use File::Path;
 use Candidate('make_directory', 'stage_directory');
+use Fcntl qw(:DEFAULT :flock);
+
+my $ops_root = "/usr/local/spk/ops";
+my $candidate_dir = "$ops_root/candidate";
+my $candidate = "candidate";
+my $rotate_conf = "rotate.conf";
+my $dir_prefix = "spk-";
+my $version_file = "$candidate_dir/version";
+
+my $scp_command = "/usr/bin/scp";
+
+my $logrotate_command = "/usr/sbin/logrotate";
+
+$EFFECTIVE_USER_ID == 0 
+    or die "You must be root to run this program\n";
+
+-d $candidate_dir
+    or die "Please create the directory '$candidate_dir', then try again\n";
+
+-f "$candidate_dir/$rotate_conf"
+    or die "\nPlease provide $candidate_dir/$rotate_conf;\n"
+           . 'it must include an entry for "candidate"'
+           . "\n(see unix manual page LOGROTATE(8))\n\n";
+
+# read version and update it atomically
+open(FH, "+< $version_file") or die "can't open $version_file\n";
+
+unless (flock(FH, LOCK_EX | LOCK_NB)) {
+    local $| = 1;
+    print "waiting for lock on $version_file...";
+    flock(FH, LOCK_EX) or die "can't lock $version_file\n";
+    print "\tOK\n";
+}
+my $version = <FH>;
+chomp $version;
+my $minor_version = $version;
+$minor_version =~ s/^.*\.//;
+$minor_version++;
+$version =~ s/[0-9]*$/$minor_version/;
+truncate(FH, 0) or die "couldn't truncate $version_file\n";
+seek(FH, 0, 0)  or die "couldn't seek to the beginning of $version_file\n";
+print FH "$version\n";
+flock(FH, LOCK_UN);
+close FH;
+
+# rotate candidates
+chdir $candidate_dir;
+
+if (-f $candidate) {
+    my @args = ($logrotate_command, "--force", "--state", "rotate.status", $rotate_conf );
+    system(@args);
+    my $exit_status = $? >> 8;
+    if ($exit_status != 0) {
+	die "logrotate failed\n";
+    }
+}
+#after rotation, determine which CRTs are still referenced
+my @dirs = <$dir_prefix*>;
+my %referenced;
+my ($f, $d);
+foreach $f (<$candidate*>) {
+    open FH, $f;
+    my $name = <FH>;
+    chomp $name;
+    $referenced{$name} = 1;
+}
+#remove any CRTs which are no longer referenced
+foreach $d (@dirs) {
+    $referenced{$d} || File::Path::rmtree $d, 0, 0;
+}
+# create the new candidate and copy files to it
+open FH, ">$candidate"
+    or die "Could not create $candidate_dir/$candidate\n";
+
+print FH "$dir_prefix$version\n";
+close FH;
+
+my $new_dir = $dir_prefix . $version;
+
+&make_directory("$new_dir/cspkserver/usr/local/bin/spktest");
+&make_directory("$new_dir/cspkserver/usr/local/include/spktest");
+&make_directory("$new_dir/cspkserver/usr/local/lib/spktest");
+&make_directory("$new_dir/cspkserver/usr/local/src/spktest");
+&make_directory("$new_dir/aspkserver/usr/local/bin/spktest");
+&make_directory("$new_dir/aspkserver/usr/local/include/spktest");
+&make_directory("$new_dir/aspkserver/usr/local/lib/spktest");
+&make_directory("$new_dir/aspkserver/usr/local/src/spktest");
+&make_directory("$new_dir/webserver/usr/local/tomcat/instance/prodssl/webapps");
+
+chdir $new_dir;
+
+&stage_directory("aspkserver", "/usr/local/bin");
+&stage_directory("aspkserver", "/usr/local/include");
+&stage_directory("aspkserver", "/usr/local/lib");
+&stage_directory("aspkserver", "/usr/local/src");
+&stage_directory("cspkserver", "/usr/local/bin");
+&stage_directory("cspkserver", "/usr/local/include");
+&stage_directory("cspkserver", "/usr/local/lib");
+&stage_directory("cspkserver", "/usr/local/src");
+
+my $srce = "webserver:/usr/local/tomcat/instance/testssl/webapps/user.war";
+my $dest =  "webserver/usr/local/tomcat/instance/prodssl/webapps/user.war";
+my @args = ($scp_command, "-r", $srce, $dest);
+system(@args);
+my $exit_status = $? >> 8;
+if ($exit_status != 0) {
+    die "'scp -r $srce $dest' failed\n";
+}
+# create a notes file to be edited by the developer
+my $notes = "$candidate_dir/notes-$version";
+open FH, "> $notes" or die "Could not create $notes\n";
+close FH;
+chmod 0666, $notes;
+print "\nPlease provide release notes for this candidate by editing the file\n";
+print "$notes\n\n";
+
+__END__
+
 
 =head1 NAME
 
@@ -69,8 +187,7 @@ use Candidate('make_directory', 'stage_directory');
 =head3 CANDIDATE REFERENCE TREE
 
     Each candidate reference tree (CRT) has a name that identifies it as
-    belonging to SPK and the date and time at which it was created.  The
-    format of the name is: spk-yyyy-mm-dd-hh-mm-ss.
+    being a version of spk. The format of the names is: spk-version
 
     The structure of each CRT is the following:
 
@@ -136,93 +253,34 @@ use Candidate('make_directory', 'stage_directory');
         is not of any use in this application, but is simply a side-effect
         of the use of logrotate.
 
+    /usr/local/spk/ops/candidate/spk-version
+
+        The actual candidate reference trees (CRTs) are directories
+        with path names that conform to the above format.
+
+    /usr/local/spk/ops/candidate/notes-version
+
+        A file to be edited by the developer of the candidate so that it 
+        contains release notes describing the changes.  At deployment,
+        all the release notes files created since the last deployment are
+        concatenated to form the release notes for the deployed version.
+
+    /usr/local/spk/ops/candidate/candidate
+     
+        Contains the file name of the most recently staged CRT
+
+    /usr/local/spk/ops/candidate/candidate.1
+
+        Contains the file name of the CRT which is the most recent save 1.
+
+     ...
+
+    /usr/local/spk/ops/candidate/candidate.n
+
+       Contains the file name of the CRT which is the most recent save n.
+
+    /usr/local/spk/ops/candidate/version
+
+       Contains the version number of the must recently staged candidate.
+
 =cut
-
-my $ops_root = "/usr/local/spk/ops";
-my $candidate_dir = "$ops_root/candidate";
-my $candidate = "candidate";
-my $rotate_conf = "rotate.conf";
-my $dir_prefix = "spk-";
-
-my $scp_command = "/usr/bin/scp";
-
-my $logrotate_command = "/usr/sbin/logrotate";
-
-$EFFECTIVE_USER_ID == 0 
-    or die "You must be root to run this program\n";
-
--d $candidate_dir
-    or die "Please create the directory '$candidate_dir', then try again\n";
-
--f "$candidate_dir/$rotate_conf"
-    or die "\nPlease provide $candidate_dir/$rotate_conf;\n"
-           . 'it must include an entry for "candidate"'
-           . "\n(see unix manual page LOGROTATE(8))\n\n";
-
-
-my ($sec, $min, $hour, $mday, $mon, $year) = localtime;
-
-my $date = sprintf "%04d-%02d-%02d-%02d%02d-%02d", $year+1900, $mon+1, $mday, $hour, $min, $sec;
-
-chdir $candidate_dir;
-
-if (-f $candidate) {
-    my @args = ($logrotate_command, "--force", "--state", "rotate.status", $rotate_conf );
-    system(@args);
-    my $exit_status = $? >> 8;
-    if ($exit_status != 0) {
-	die "logrotate failed\n";
-    }
-}
-my @dirs = <$dir_prefix*>;
-my %referenced;
-my ($f, $d);
-foreach $f (<$candidate*>) {
-    open FH, $f;
-    my $name = <FH>;
-    chomp $name;
-    $referenced{$name} = 1;
-}
-
-foreach $d (@dirs) {
-    $referenced{$d} || File::Path::rmtree $d, 0, 0;
-}
-
-open FH, ">$candidate"
-    or die "Could not create $candidate_dir/$candidate\n";
-
-print FH "$dir_prefix$date\n";
-close FH;
-
-my $new_dir = $dir_prefix . $date;
-
-&make_directory("$new_dir/cspkserver/usr/local/bin/spktest");
-&make_directory("$new_dir/cspkserver/usr/local/include/spktest");
-&make_directory("$new_dir/cspkserver/usr/local/lib/spktest");
-&make_directory("$new_dir/cspkserver/usr/local/src/spktest");
-&make_directory("$new_dir/aspkserver/usr/local/bin/spktest");
-&make_directory("$new_dir/aspkserver/usr/local/include/spktest");
-&make_directory("$new_dir/aspkserver/usr/local/lib/spktest");
-&make_directory("$new_dir/aspkserver/usr/local/src/spktest");
-&make_directory("$new_dir/webserver/usr/local/tomcat/instance/prodssl/webapps");
-
-chdir $new_dir;
-
-&stage_directory("aspkserver", "/usr/local/bin");
-&stage_directory("aspkserver", "/usr/local/include");
-&stage_directory("aspkserver", "/usr/local/lib");
-&stage_directory("aspkserver", "/usr/local/src");
-&stage_directory("cspkserver", "/usr/local/bin");
-&stage_directory("cspkserver", "/usr/local/include");
-&stage_directory("cspkserver", "/usr/local/lib");
-&stage_directory("cspkserver", "/usr/local/src");
-
-my $srce = "webserver:/usr/local/tomcat/instance/testssl/webapps/user.war";
-my $dest =  "webserver/usr/local/tomcat/instance/prodssl/webapps/user.war";
-my @args = ($scp_command, "-r", $srce, $dest);
-system(@args);
-my $exit_status = $? >> 8;
-if ($exit_status != 0) {
-    die "'scp -r $srce $dest' failed\n";
-}
-
