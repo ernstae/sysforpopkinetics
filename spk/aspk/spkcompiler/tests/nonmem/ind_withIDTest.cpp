8b9f4a83a5d4d871e2642c39fa3466565f59695f
honda <honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2004-11-02 19:22:46 +0000

    Linking with libspk.

--- spk/aspk/spkcompiler/tests/nonmem/ind_withIDTest.cpp
+++ spk/aspk/spkcompiler/tests/nonmem/ind_withIDTest.cpp
@@ -123,6 +123,7 @@ namespace{
   char CLAPACKLIB[] = "atlas";
   char PTHREADLIB[] = "pthread";
   char MLIB[]       = "m";
+  char XERCESCLIB[] = "xerces-c";
   char LDPATH[]     = "-Wl,--rpath -Wl,/usr/local/lib/spktest -L/usr/local/lib/spktest";
   char CPPFLAG[]    = "-g -I/usr/local/include/spktest";
   char LDFLAG[514];
@@ -460,8 +461,8 @@ void ind_withIDTest::setUp()
   sprintf( fPredDriver,           "%s_PredDriver",           fPrefix );
   sprintf( fPredDriver_cpp,       "%s_PredDriver.cpp",       fPrefix );
 
-  sprintf( LDFLAG, "%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s",
-	   LDPATH, SPKLIB, SPKPREDLIB, SPKOPTLIB, ATLASLIB, CBLASLIB, CLAPACKLIB, PTHREADLIB, MLIB );
+  sprintf( LDFLAG, "%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s",
+	   LDPATH, SPKLIB, SPKPREDLIB, SPKOPTLIB, ATLASLIB, CBLASLIB, CLAPACKLIB, PTHREADLIB, MLIB, XERCESCLIB );
 
   // ID doesn't have an alias
   label_alias[strID]   = NULL;
--- spk/aspk/spkcompiler/tests/nonmem/ind_withoutIDTest.cpp
+++ spk/aspk/spkcompiler/tests/nonmem/ind_withoutIDTest.cpp
@@ -124,6 +124,7 @@ namespace{
   char CLAPACKLIB[] = "atlas";
   char PTHREADLIB[] = "pthread";
   char MLIB[]       = "m";
+  char XERCESCLIB[] = "xerces-c";
   char LDPATH[]     = "-Wl,--rpath -Wl,/usr/local/lib/spktest -L/usr/local/lib/spktest";
   char CPPFLAG[]    = "-g -I/usr/local/include/spktest";
   char LDFLAG[514];
@@ -455,8 +456,8 @@ void ind_withoutIDTest::setUp()
   sprintf( fPredDriver,           "%s_PredDriver",           fPrefix );
   sprintf( fPredDriver_cpp,       "%s_PredDriver.cpp",       fPrefix );
 
-  sprintf( LDFLAG, "%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s",
-	   LDPATH, SPKLIB, SPKPREDLIB, SPKOPTLIB, ATLASLIB, CBLASLIB, CLAPACKLIB, PTHREADLIB, MLIB );
+  sprintf( LDFLAG, "%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s -l%s",
+	   LDPATH, SPKLIB, SPKPREDLIB, SPKOPTLIB, ATLASLIB, CBLASLIB, CLAPACKLIB, PTHREADLIB, MLIB, XERCESCLIB );
 
   // TIME doesn't have an alias
   label_alias[strTIME] = NULL;
