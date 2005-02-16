f363293268af5f42d52f4b6a065f2e82cb4e2cfc
alan <alan@09616d79-e138-8681-36ae-a2fdb6c30477>
2005-02-16 18:45:29 +0000

    Changes to paths.

--- src/misc/parallel/newjob.sh
+++ src/misc/parallel/newjob.sh
@@ -1,7 +1,33 @@
-SHARE=/usr/local/spk/share
-ARCH=i686
+#!/bin/bash
+
+TMP=/tmp
+NEXT_JOB=$TMP/demo_next_job
+let n=1
+
+if [ -z $SPK_SHARE ];
+then
+    SPK_SHARE=/usr/local/spk/share
+fi
+ARCH=LINUXX86_64   # the architecture of the head node
 MODE=test
-mkdir $SHARE/working/spk$MODE/spkjob-$1
-$SHARE/arch/$ARCH/bin/spk$MODE/spkjob $1 $2 $MODE
+
+if [ -n "$1" ] && [ "$1" = "-r" ];  # reset
+then
+    rm -rf $SPK_SHARE/working/spktest/*
+    rm -f $SPK_SHARE/log/spktest/*
+    rm -f $NEXT_JOB
+fi
+
+if [ -f $NEXT_JOB ];
+then
+    n=`cat $NEXT_JOB`
+    let n=$n+1
+fi
+printf "%06d" $N > $NEXT_JOB
+printf "%06d" $N
+printf "\n"
+
+#mkdir $SPK_SHARE/working/spk$MODE/spkjob-$1
+#$SPK_SHARE/arch/bin/$ARCH/spk$MODE/spkjob $1 $2 $MODE
 
 
