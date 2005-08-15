9e35c4aeab2722e13af9d2d9e25e595a0b5d5dc6
honda <honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2005-08-15 08:30:21 +0000

    getInitialOff(), getNoOff() and getNoDose() are given non-const vectors to return values.

--- trunk/spk/aspk/spkcompiler/spkcompiler/nonmem/CompModelInfo.h
+++ trunk/spk/aspk/spkcompiler/spkcompiler/nonmem/CompModelInfo.h
@@ -66,24 +66,72 @@ class CompModelInfo{
    * Get the number of compartments in this system.
    */
   int getNCompartments() const;
+
   /**
    * Get the number of basic PK parameters associated with this system.
    */
   int getNParameters() const;
+
   /**
    * Get the number of equilibrim compartments in this system.
    */
   int getNEquilibrims() const;
+ 
+  /**
+   * True if the PK block is a function of T (continuous time).
+   */
+  bool is_pkFunctionOfT() const;
+
+  /**
+   * Set as to whether the PK block is a function of T or not.
+   */
+  void setPkFunctionOfT( bool );
+  
+  /**
+   * Returns the number of compartment that is set to receive dose.
+   * The first compartment (=0) is set by default.
+   */
+  int getDefaultDose() const;
+
+  /**
+   * Returns the number of compartment that is set to be the
+   * observation compartment.  The first compartment (=0)
+   * is set by default.
+   */
+  int getDefaultObservation() const;
+
+  /**
+   * Returns a vector of boolean values where the i-th element
+   * indicates as to whether
+   * the corresponding compartment is to be turned off initially.
+   * True indicates turn-off.
+   */
+  void getInitialOff( std::vector<bool>& initial_off ) const;
+
+  /**
+   * Returns a vector of boolean values where the i-th element
+   * indicates as to whether the corresponding compartment
+   * can be turn off or on.  True indicates no turn off.
+   */
+  void getNoOff( std::vector<bool>& no_off ) const;
+
+  /**
+   * Returns a vector of boolean values, where the i-th element
+   * indicates as to whether the corresponding compartment
+   * can receive dose or not.  True indicates no dose.
+   */
+  void getNoDose( std::vector<bool>& no_dose ) const;
+
   /**
    * Get the i-th Compartment object.
    * @param i The number/order of the object.
    */
-  const CompartmentInfo operator[]( int i ) const;
+  const CompartmentInfo& operator[]( int i ) const;
   /**
    * Get the i-th CompartmentInfo object.
    * @param i The number/order of the object.
    */
-  CompartmentInfo operator[]( int i );
+  CompartmentInfo& operator[]( int i );
   /**
    *
    */
@@ -95,6 +143,7 @@ class CompModelInfo{
   int nCompartments;
   int nParameters;
   int nEquilibrims;
+  bool isPkFunctionOfT;
   std::vector<CompartmentInfo> compartments;
 };
 
