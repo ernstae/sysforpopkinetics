7c7b5543ddbb43d26cfacb8f68c30aa78bfba511
honda <honda@09616d79-e138-8681-36ae-a2fdb6c30477>
2005-10-11 23:57:08 +0000

    *** empty log message ***

---
+++ trunk/spk/aspk/spkcompiler/tests/nonmem/pop_insertDataItemsTest.h
@@ -0,0 +1,63 @@
+/**
+ * \file pop_insertDataItemsTest.h
+ * \brief Tests the NonmemTranslator's ability to handle the
+ * data sets missing AMT, MDV or/and EVID.
+ */
+#ifndef POP_INSERTDATAITEMS_TEST_H
+#define POP_INSERTDATAITEMS_TEST_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestSuite.h>
+
+#include <xercesc/dom/DOMDocument.hpp>
+#include <xercesc/parsers/XercesDOMParser.hpp>
+#include "../../spkcompiler/nonmem/NonmemTranslator.h"
+
+#include <vector>
+
+class pop_insertDataItemsTest 
+: public CppUnit::TestFixture,
+  public NonmemTranslator
+{
+
+  xercesc::DOMDocument *source;
+  xercesc::DOMDocument *data;
+  xercesc::DOMDocument *report;
+
+  xercesc::XercesDOMParser * dataParser;
+  xercesc::XercesDOMParser * sourceParser;
+  
+  bool okToClean;
+public:
+    static CppUnit::Test * suite();
+    virtual void setUp();
+    virtual void tearDown();
+
+    void createDataML  ( const char * fDataML, 
+			 int nIndividuals,
+			 int nItems, 
+			 const char* label[],
+			 int nRecords, 
+			 const std::vector< std::vector<double> > & set );
+    void parseDataML   ( xercesc::XercesDOMParser *dataParser, 
+			 const char * fDataML,
+			 int nIndividuals );
+    void createSourceML( const char * fSourceML,
+			 int nIndividuals,
+			 int nLabels, 
+			 const char* label[] );
+    void parseSourceML ( xercesc::XercesDOMParser *sourceParser, 
+			 const char* fSourceML,
+			 int nIndividuals );
+
+    void noAMT_noMDV_noEVID();
+    void noAMT_noMDV_yesEVID();
+    void noAMT_yesMDV_noEVID();
+    void noAMT_yesMDV_yesEVID();
+    void yesAMT_noMDV_noEVID();
+    void yesAMT_noMDV_yesEVID();
+    void yesAMT_yesMDV_noEVID();
+    void yesAMT_yesMDV_yesEVID();
+};
+
+#endif
