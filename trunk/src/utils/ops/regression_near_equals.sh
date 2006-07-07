d6e3c521d56541360caa52b567cda29da934cad9
ernst <ernst@09616d79-e138-8681-36ae-a2fdb6c30477>
2006-07-07 22:25:53 +0000

    Used in the regression test to strip out characters that are non-numeric and test numeric values against known results.

--- trunk/src/utils/ops/regression_near_equals.sh
+++ trunk/src/utils/ops/regression_near_equals.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-#
+# take the good test and write to a temp file
+# take the new test results and write to another temp file
+
+GOODFILE=/tmp/regression_test_good_$$.txt
+NEWFILE=/tmp/regression_test_new_$$.txt
+
+sed -e 's|,| |g' -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' -e 's|<[^>]*>.*</[^>]*>||g' $1 | sed -n -e 's|^ *$||' -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' > $GOODFILE
+
+sed -e 's|,| |g' -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' -e 's|<[^>]*>.*</[^>]*>||g' $2 | sed -n -e 's|^ *$||' -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' > $NEWFILE
+
+/usr/local/bin/NearEqual $GOODFILE $NEWFILE $3 $4
+
+RETURN_STATUS=$?
+
+rm -rf $GOODFILE $NEWFILE
+
+exit $RETURN_STATUS
+
+
+
