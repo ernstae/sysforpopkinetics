9d288c920979de17b59bc34f710a0100de151966
Andrew Ernst <ernstae@users.noreply.github.com>
2006-07-08 00:01:30 +0000

    Fixed for case of '------'

--- src/utils/ops/regression_near_equals.sh
+++ src/utils/ops/regression_near_equals.sh
@@ -6,16 +6,33 @@
 GOODFILE=/tmp/regression_test_good_$$.txt
 NEWFILE=/tmp/regression_test_new_$$.txt
 
-sed -e 's|,| |g' -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' -e 's|<[^>]*>.*</[^>]*>||g' $1 | sed -n -e 's|^ *$||' -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' > $GOODFILE
+sed -e 's|,| |g' 
+\    -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' 
+\    -e 's|<[^>]*>.*</[^>]*>||g' $1 
+| sed -n 
+\    -e 's|^ *$||' 
+\     -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' 
+\     -e 's|^-* ||g' 
+\     -e 's| -*$||g' 
+\     -e 's| -* ||g' > $GOODFILE
 
-sed -e 's|,| |g' -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' -e 's|<[^>]*>.*</[^>]*>||g' $2 | sed -n -e 's|^ *$||' -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' > $NEWFILE
+sed -e 's|,| |g' 
+\    -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' 
+\    -e 's|<[^>]*>.*</[^>]*>||g' $1 
+| sed -n 
+\    -e 's|^ *$||' 
+\     -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' 
+\     -e 's|^-* ||g' 
+\     -e 's| -*$||g' 
+\     -e 's| -* ||g' > $NEWFILE
 
 /usr/local/bin/NearEqual $GOODFILE $NEWFILE $3 $4
 
 RETURN_STATUS=$?
 
-rm -rf $GOODFILE $NEWFILE
+#rm -rf $GOODFILE $NEWFILE
 
+echo $RETURN_STATUS
 exit $RETURN_STATUS
 
 
