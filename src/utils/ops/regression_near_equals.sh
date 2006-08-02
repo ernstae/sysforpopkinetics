3991d655a5c55e061d6fd7c9d427deed6400f5b8
Andrew Ernst <ernstae@users.noreply.github.com>
2006-08-02 18:54:26 +0000

    *** empty log message ***

--- src/utils/ops/regression_near_equals.sh
+++ src/utils/ops/regression_near_equals.sh
@@ -6,34 +6,23 @@
 GOODFILE=/tmp/regression_test_good_$$.txt
 NEWFILE=/tmp/regression_test_new_$$.txt
 
-sed -e 's|,| |g' 
-\    -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' 
-\    -e 's|<[^>]*>.*</[^>]*>||g' $1 
-| sed -n 
-\    -e 's|^ *$||' 
-\     -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' 
-\     -e 's|^-* ||g' 
-\     -e 's| -*$||g' 
-\     -e 's| -* ||g' > $GOODFILE
+sed -e 's|,| |g' -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' -e 's|<[^>]*>.*</[^>]*>||g' $1  | sed -n -e 's|^ *$||' -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' -e 's|^-* ||g'  -e 's| -*$||g' -e 's| -* ||g' > $GOODFILE;
 
-sed -e 's|,| |g' 
-\    -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' 
-\    -e 's|<[^>]*>.*</[^>]*>||g' $1 
-| sed -n 
-\    -e 's|^ *$||' 
-\     -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' 
-\     -e 's|^-* ||g' 
-\     -e 's| -*$||g' 
-\     -e 's| -* ||g' > $NEWFILE
+sed -e 's|,| |g' -e 's|<[^>]*>\([ 0-9.e+-]*\)</[^>]*>|\n\1\n|g' -e 's|<[^>]*>.*</[^>]*>||g' $2  | sed -n -e 's|^ *$||' -e '/^[ 0-9.e+-][ 0-9.e+-]*$/p' -e 's|^-* ||g'  -e 's| -*$||g' -e 's| -* ||g' > $NEWFILE;
 
-/usr/local/bin/NearEqual $GOODFILE $NEWFILE $3 $4
+/usr/local/bin/NearEqual $GOODFILE $NEWFILE $3 $4 $5
 
-RETURN_STATUS=$?
+if [ $?  ]
+then
+  exit 1
+else
+  exit 0
+fi
 
 #rm -rf $GOODFILE $NEWFILE
 
-echo $RETURN_STATUS
-exit $RETURN_STATUS
+#echo $RETURN_STATUS
+
 
 
 
