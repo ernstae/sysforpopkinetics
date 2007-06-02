3e9b00687446e178e46c7686ae599f624a4888f0
Andrew Ernst <ernstae@users.noreply.github.com>
2007-06-02 01:40:15 +0000

    This fixed a problem with the SQL query->row conversion

--- sgid/web/html/runjob.php
+++ sgid/web/html/runjob.php
@@ -1,15 +1,18 @@
 <?
-$debug = 1;
 
 require_once ("conf/SGID.php");
 
-$jobid = htmlentities($_REQUEST['jobid']);
-
 if ( isset($_REQUEST['jobid']) && strlen($_REQUEST['jobid']) > 0 )
   {
-    $results = $db->query("SELECT email_address, equations, seed FROM job WHERE md5(concat(id,seed))=" . $jobid);
+    $jobid = htmlentities($_REQUEST['jobid']);
     
-    if ( $db->fetchInto($row) ) 
+    $_SESSION['jobid'] = $jobid;
+    $_SESSION['step'] = 0;
+    
+    
+    $results = $db->query("SELECT * FROM job WHERE md5(concat(id,seed))='" . $jobid . "'");
+    
+    if ( $results->fetchInto($row) ) 
       {
 	$_SESSION['email_address'] = $row->email_address;
 	$_SESSION['seed'] = $row->seed;
