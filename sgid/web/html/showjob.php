679fe66dda712ec8028656d78a202d2ec5ac2245
Andrew Ernst <ernstae@users.noreply.github.com>
2007-06-06 20:06:42 +0000

    New versions to support algebraic equations

--- sgid/web/html/showjob.php
+++ sgid/web/html/showjob.php
@@ -20,11 +20,9 @@ if ( isset($_REQUEST['jobid']) && strlen($_REQUEST['jobid']) > 0 ) {
  }
 
 
-
-
 $title = "Job status for SGID job: " . $jobid;
 
-$results = $db->query("SELECT equations, result_xml, id, state_code, end_code, equations FROM job WHERE md5(concat(id,seed))='" . $jobid . "'");
+$results = $db->query("SELECT equations, result_xml, id, state_code, end_code FROM job WHERE md5(concat(id,seed))='" . $jobid . "'");
 
 //echo "<pre>" . var_dump($results) . "</pre>\n";
 
@@ -42,14 +40,20 @@ if ( PEAR::isError($results) ) {
 <h3><?= $GLOBALS['OPTIONS']['site_name'] ?></h3>
 
 <? 
-  if ( $results->numRows() > 0 ) {
-    $row = $results->fetchRow();
-    $xml = SGID_getXML($row->result_xml);
-    $job_id = $row->id;  }
-  else {
-    $job_id = "Unavailable";
-    add_error("QUEUE","The job you have requested does not exist.");
-  }
+  $show_results = false;
+
+  if ( $results->numRows() > 0 ) 
+    {
+      $row = $results->fetchRow();
+      $xml = SGID_getXML($row->result_xml);
+      $job_id = $row->id;  
+      $show_results = true;
+    }
+  else 
+    {
+      $job_id = "Unavailable";
+      add_error("QUEUE","The job you have requested does not exist.");
+    }
 
 ?>
 
@@ -64,54 +68,61 @@ if ( PEAR::isError($results) ) {
 
 <legend>Job Results : <?= $job_id ?></legend>
 
-  <?    if ( $row->state_code == 'run' || $row->state_code == 'queue' ) 
+<?    
+    if ( $row->state_code == 'run' || $row->state_code == 'queue' ) 
       { 
 	echo "Your job has not completed running.  It is currently in the " . $row->state_code . " state.\n";
       } 
- else 
-   {
-     if ( $row->end_code != 'srun' || strlen($xml['error_messages']) > 0 ) {
-       // we need to show an error block
-     ?>
+elseif ( $row->state_code == 'end' ) 
+{
+  if ( isset($xml) && ($row->end_code != 'srun' || strlen($xml['error_messages']) > 0 )) {
+    // we need to show an error block
+    ?>
+    
+    <ol>
+      <b>Errors Were Detected: </b>
+      <ul>
+      <pre>
+      <?= $xml['error_messages'] ?>
+      </pre>
+      </ul>
+      
+      <? } // end of error block ?>
+  <ol>
+     <b>Solutions Found</b>:
+  <ul>
+     <pre><?= $xml['number_of_solutions'] ?> <?= $xml['status_of_the_solutions'] ?></pre>
+     </ul>
+     <b>Processing Time</b>:
+  <ul><pre><?= $xml['elapsed_seconds'] ?> second(s)</pre></ul>
+     
+     
+     <b>Parameter Seed</b>:
+  <ul>
+     <pre><?= $xml['parameter_seed'] ?></pre>
+     </ul>
+     
+     <b>Computation Information</b>:
+  <ul>
+     <pre>
+     <?= eregi_replace("\n{3,100}", "\n", htmlentities($xml['calculation_details'])) ?>
+     </pre>
+     </ul>
+     
+     <b>Your input equations were</b>:
+  <ul>
+     <pre><?= htmlentities($row->equations) ?></pre>
+     </ul>
 
-<ol>
-<b>Errors Were Detected: </b>
-<ul>
-<pre>
-<?= $xml['error_messages'] ?>
-</pre>
-</ul>
+<?
 
-<? } // end of error block ?>
-<ol>
-   <b>Solutions Found</b>:
-<ul>
-<pre><?= $xml['number_of_solutions'] ?> <?= $xml['status_of_the_solutions'] ?></pre>
-</ul>
-<b>Processing Time</b>:
-<ul><pre><?= $xml['elapsed_seconds'] ?> second(s)</pre></ul>
+     }  // elseif
+ else {
+   echo "We have no record of the job you are requesting.  Please try again.";
+ }
 
 
-<b>Parameter Seed</b>:
-<ul>
-<pre><?= $xml['parameter_seed'] ?></pre>
-</ul>
-
-<b>Computation Information</b>:
-<ul>
-<pre>
-<?= eregi_replace("\n{3,100}", "\n", htmlentities($xml['calculation_details'])) ?>
-</pre>
-  </ul>
-
-   <b>Your input equations were</b>:
-<ul>
-<pre>
-<?= htmlentities($row->equations) ?>
-</pre>
-</ul>
-
-   <? } // else ?>
+?>
 </fieldset>
 </form>
 
