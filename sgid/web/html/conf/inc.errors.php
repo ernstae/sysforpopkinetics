00d5fb17851f33034fa96f9f7e9da4a1cf7eb4f3
Andrew Ernst <ernstae@users.noreply.github.com>
2007-08-29 01:04:13 +0000

    Fixes bug 756 : Counting outputs

--- sgid/web/html/conf/inc.errors.php
+++ sgid/web/html/conf/inc.errors.php
@@ -16,6 +16,12 @@ $errors =
 	    => 'Your system-experiment model has only a single input. When there is only a single input, it must be named U.   Try this model again with U for your input.  I found',
 	    'parse_illegal'
 	    => 'The following illegal character was found',
+	    'parse_out_Ymissing'
+	    => 'Your system-experiment model has only a single output.  When ther eis only a single output, it must be named Y.',
+	    'parse_out_Ymulti'
+	    => 'Your system-experiment model has multiple outputs.  When there are multiple outputs, they must be named {Y1, Y2, Y3, .., Yn}.',
+	    'parse_out'
+	    => 'No outputs were defined.  Please try again and enter one or more output equations',
 	    'parse'
 	    => '',
 	    'parse_exponent'
