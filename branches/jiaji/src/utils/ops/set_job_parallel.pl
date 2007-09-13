f785b53b04a8763f4e2150dd978907f1877c65c9
jiaji <jiaji@09616d79-e138-8681-36ae-a2fdb6c30477>
2007-09-13 21:08:06 +0000

    for parallel

---
+++ branches/jiaji/src/utils/ops/set_job_parallel.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Spkdb ('connect', 'disconnect', 'set_parallel');
+
+=head1 NAME
+
+    set_parallel.pl -- set flags in spktest database to make the jobs 
+    run in parallel processing mode
+
+=head1 SYNOPSIS
+
+    set_parallel.pl job_id ...
+
+    =head1 DESCRIPTION
+
+    Given a list of job_id numbers, this program sets flags in the job table 
+    of the spktest database to make the jobs run in parallel processing mode.
+
+=head1 DEPENDENCIES
+
+    It uses the SPK Perl API module:
+
+    Spkdb.pm
+
+=cut
+
+my $dbh = &connect("spktest", "dbserver", "tester", "tester");
+for my $job_id (@alljobs) {
+    &set_parallel($dbh, $job_id, 1);
+}
+&disconnect($dbh);
