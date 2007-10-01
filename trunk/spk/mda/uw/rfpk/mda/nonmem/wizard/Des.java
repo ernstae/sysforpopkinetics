091a16a5ba886f5603b2f8f3e5e6ae9ae3d58916
jiaji <jiaji@09616d79-e138-8681-36ae-a2fdb6c30477>
2007-10-01 20:59:46 +0000

    fixed bug of warning blocking

--- trunk/spk/mda/uw/rfpk/mda/nonmem/wizard/Des.java
+++ trunk/spk/mda/uw/rfpk/mda/nonmem/wizard/Des.java
@@ -303,6 +303,8 @@ public class Des extends javax.swing.JPanel implements WizardStep {
                     {
                         JOptionPane.showMessageDialog(null, e, "BadLocationException", JOptionPane.ERROR_MESSAGE);
                     }
+                    if(lines.size() == 0 && errors.size() == 0)
+                        return true;
                     return false;
                 }
             }
